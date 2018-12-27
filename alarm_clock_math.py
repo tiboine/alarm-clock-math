547d1134e48816d776e77e6a4363c5044d9774ed
tiboine <34999988+tiboine@users.noreply.github.com>
2018-12-27 19:13:54 +0100

    Update alarm_clock_math.py

--- alarm_clock_math.py
+++ alarm_clock_math.py
@@ -10,9 +10,9 @@ answers = [0,0,0,0]
 
 for num in range(4):
 	
-	first = random.choice(random_int)
-	symbol = random.choice(symbols)
-	last = random.choice(random_int)
+	first = random.choice(random_int) # first number
+	symbol = random.choice(symbols) # arithmetic 
+	last = random.choice(random_int) # last number
 	
 	
 	print(f'{x+1} {first} {symbol} {last}')
@@ -33,10 +33,13 @@ for num in range(4):
 	
 answer_random = random.randint(0,3)
 print('')
-print(answers[answer_random])
+print(answers[answer_random]) # print answer to the equation to guess
 
 user = input('Guess 1-4 : ')
 if user == str(answer_random+1):
 	print('Good boy. Alarm off')
 else:
 	print('Nope. try again')
+
+# TODO: 
+# repeat app when wrong answer given
