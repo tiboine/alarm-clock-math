7a789a9b362f8c1daeb816ea7821cd77bd11b627
tiboine <34999988+tiboine@users.noreply.github.com>
2018-12-27 18:44:32 +0100

    Initial commit

---
+++ alarm_clock_math.py
@@ -0,0 +1,51 @@
+# show 4 sets of equations. 
+#guess the right equation for the answer
+
+import random
+x = 0
+random_int = list(range(1,99))
+symbols = ['+', '-', '/', '*']
+answers = [0,0,0,0]
+
+
+for num in range(4):
+	
+	first = random.choice(random_int)
+	symbol = random.choice(symbols)
+	last = random.choice(random_int)
+	
+	
+	print(f'{x+1} {first} {symbol} {last}')
+	
+	if symbol == '+':
+		answers[x] = first + last
+		x += 1
+	if symbol == '-':
+		answers[x] = first - last
+		x += 1
+	if symbol == '/':
+		answers[x] = round(first / last,2)
+		x += 1
+	if symbol == '*':
+		answers[x] = first * last
+		x += 1
+
+	
+answer_random = random.randint(0,3)
+print('')
+print(answers[answer_random])
+
+
+user = input('your guess: ')
+if user == str(answer_random+1):
+	print('wee')
+else:
+	print('nooooo')
+
+
+
+
+
+
+
+
